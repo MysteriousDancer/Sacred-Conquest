9f4af595d9379b46a36fe5232824b168252f2894
MysteriousDancer <76506552+MysteriousDancer@users.noreply.github.com>
2021-01-22 18:55:47 -0600

    starting event

---
+++ Graphics/Statscreen/Statscreen.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(Statscreen)
+
+//======================================================================
+//
+//	Statscreen, 256x160@8, 
+//	+ palette 256 entries, not compressed
+//	+ 640 tiles not compressed
+//	Total size: 512 + 40960 = 41472
+//
+//	Time-stamp: 2021-01-22, 18:49:02
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_STATSCREEN_H
+#define GRIT_STATSCREEN_H
+
+#define StatscreenTilesLen 40960
+extern const unsigned short StatscreenTiles[20480];
+
+#define StatscreenPalLen 512
+extern const unsigned short StatscreenPal[256];
+
+#endif // GRIT_STATSCREEN_H
+
+//}}BLOCK(Statscreen)
